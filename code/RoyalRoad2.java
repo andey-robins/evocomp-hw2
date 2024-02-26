70a43f6d613cd8c00753054f4cb46014babf4141
Darren Bansil <106610067+libioco@users.noreply.github.com>
2024-02-26 18:03:25 -0500

    Implement RR2

---
+++ code/RoyalRoad2.java
@@ -0,0 +1,60 @@
+
+/******************************************************************************
+*  A Teaching GA					  Developed by Hal Stringer & Annie Wu, UCF
+*  Version 2, January 18, 2004
+*******************************************************************************/
+
+import java.io.*;
+
+public class RoyalRoad2 implements IFitnessFunction {
+
+    private static String name = "RoyalRoad2 Problem";
+
+    private Schema[] schemata = new Schema[] {
+            new Schema("11111111********************************************************", 8),
+            new Schema("********11111111************************************************", 8),
+            new Schema("****************11111111****************************************", 8),
+            new Schema("************************11111111********************************", 8),
+            new Schema("********************************11111111************************", 8),
+            new Schema("****************************************11111111****************", 8),
+            new Schema("************************************************11111111********", 8),
+            new Schema("********************************************************11111111", 8),
+            new Schema("1111111111111111************************************************", 16),
+            new Schema("****************1111111111111111********************************", 16),
+            new Schema("********************************1111111111111111****************", 16),
+            new Schema("************************************************1111111111111111", 16),
+            new Schema("11111111111111111111111111111111********************************", 32),
+            new Schema("********************************11111111111111111111111111111111", 32),
+    };
+
+    public RoyalRoad2() {
+    }
+
+    // COMPUTE A CHROMOSOME'S RAW FITNESS
+    public void doRawFitness(Chromo X) {
+        X.rawFitness = 0;
+        for (int i = 0; i < this.schemata.length; i++) {
+            X.rawFitness += this.schemata[i].ScoreString(X.chromo);
+        }
+    }
+
+    // PRINT OUT AN INDIVIDUAL GENE TO THE SUMMARY FILE
+    public void doPrintGenes(Chromo X, FileWriter output) throws IOException {
+
+        for (int i = 0; i < Parameters.numGenes; i++) {
+            halio.Hwrite.right(X.getGeneAlpha(i), 11, output);
+        }
+        output.write("   RawFitness");
+        output.write("\n        ");
+        for (int i = 0; i < Parameters.numGenes; i++) {
+            halio.Hwrite.right(X.getPosIntGeneValue(i), 11, output);
+        }
+        halio.Hwrite.right((int) X.rawFitness, 13, output);
+        output.write("\n\n");
+        return;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
